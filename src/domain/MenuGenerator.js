c5b15199ea455b55547697f88700e72f09fd7319
mieum12 <qpdlqltb1215@gmail.com>
2023-12-09 23:01:33 +0900

    fix: 셔플에 숫자로 변환해 돌리기 에러 해결

--- src/domain/MenuGenerator.js
+++ src/domain/MenuGenerator.js
@@ -7,8 +7,14 @@ class MenuGenerator {
    * @return {MenuItem}
    */
   generateByCategory(category) {
+    // 해당 카테고리의 메뉴들 9가지
     const menuItems = Menu.findMenusByCategory(category);
-    return Random.shuffle(menuItems)[0];
+    // 메뉴를 숫자(인덱스)로 변환 [0,1,2, ...,8]
+    const indexArr = menuItems.map((_, index) => index);
+    // 인덱스 랜덤 뽑기
+    const randomMenuIndex = Random.shuffle(indexArr)[0];
+    // 해당인덱스의 메뉴를 리턴
+    return menuItems[randomMenuIndex];
   }
 }
 
